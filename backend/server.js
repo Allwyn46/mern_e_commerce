614afe56bc616265c927128b6cbc2b2adcb0ffdb
Allwyn46 <allwyn153@gmail.com>
2024-09-04 20:36:32 +0530

    DB and CORS Config done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+console.log
 
 mongoose
-  .connect(
-    `mongodb+srv://super_admin_commerce:${process.env.MONGOOSE_PASS}@ecommerce.pwekm.mongodb.net/`
-  )
-  .then(() => console.log("Database Connected Successfully"))
-  .catch((error) => console.error(error));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('Database Connected Successfully'))
+    .catch((error) => console.error(error))
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND,
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma',
+        ],
+        credentials: true,
+    })
+)
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`server is now running ${PORT}`))
