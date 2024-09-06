276c71f5370731c274e232fb0dbe30ca8f1726db
Allwyn46 <allwyn153@gmail.com>
2024-09-06 20:36:23 +0530

    created admin layout

---
+++ frontend/src/pages/admin-panel/AdminLayout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import AdminSidebar from './AdminSidebar'
+import AdminHeader from './AdminHeader'
+
+const AdminLayout = () => {
+    return (
+        <div className='flex min-h-screen w-full'>
+
+            {/* SIDEBAR */}
+            <AdminSidebar />
+
+            <div className='flex flex-1 flex-col'>
+                
+                {/* HEADER */}
+                <AdminHeader />
+
+                <main className='flex-1 flex bg-muted/40 p-4 md:p-6'>
+                    <Outlet />
+                </main>
+
+            </div>
+        </div>
+    )
+}
+
+export default AdminLayout
