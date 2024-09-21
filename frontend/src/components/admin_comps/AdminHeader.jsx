8213c11ec975722b9932f0de8c9c65ab71f4a458
Allwyn <allwyn153@gmail.com>
2024-09-21 13:37:30 +0530

    completed admin header

--- frontend/src/components/admin_comps/AdminHeader.jsx
+++ frontend/src/components/admin_comps/AdminHeader.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { LogOut, Menu } from 'lucide-react'
 
 const AdminHeader = () => {
     return (
-        <div>AdminHeader</div>
+        <header className='flex justify-center items-center px-4 py-3 bg-background border-b'>
+            <Button className="lg:hidden sm:block">
+                <Menu />
+            </Button>
+            <span className='sr-only'>Togglemenu</span>
+            <div className='flex flex-1 justify-end'>
+                <Button variant="outline" className="inline-flex gap-2 items-center rounded-md px-4 py-2"><LogOut />Logout</Button>
+            </div>
+        </header>
     )
 }
 
